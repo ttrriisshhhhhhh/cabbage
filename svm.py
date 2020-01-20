e80aae543155220b771fb89b9773252c27461b97
Ymyel John Sivila <ymyelsivila@gmail.com>
2020-01-20 21:20:58 +0800

    SVM

--- svm.py
+++ svm.py
@@ -33,7 +33,7 @@ def load_image_files(container_path, dimension=(64, 64)):
                  images=images,
                  DESCR=descr)
 
-image_dataset = load_image_files("cabbage/data/original")
+image_dataset = load_image_files("cabbage/data/dataset")
 
 X_train, X_test, y_train, y_test = train_test_split(
     image_dataset.data, image_dataset.target, test_size=0.3,random_state=109)
