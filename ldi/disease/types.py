2855786c18ff98d5b587fcee81071950ce261228
juanpransisko <olivo.jfrancis@gmail.com>
2020-01-26 08:17:15 +0800

    disease classes

---
+++ ldi/disease/types.py
@@ -0,0 +1,27 @@
+import pandas
+
+
+class Disease:
+    """ Holds available list of diseases for classification in 
+        cabbage leaves
+    """
+
+    def __init__(self):
+        import os
+        __types__ = "ldi/disease/class_disease_list.csv"
+        self.classes = self.read_data(__types__)
+
+    def read_data(self, file):
+        return pandas.read_csv(file)
+
+    def __add__(self, name):
+        raise NotImplementedError
+        self.classes.append(name)
+
+    def __remove__(self, name):
+        raise NotImplementedError
+        self.classes.remove(name)
+
+    def __list__(self):
+        from pprint import pprint
+        pprint(self.classes)
